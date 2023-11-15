729a6509ae4d662376ef9f34bd2e943d07807236
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2023-11-15 13:45:15 +0700

    add  base for HomeController

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -13,6 +13,11 @@ class HomeController extends BaseController
 
     public function index()
     {
-        return $this->view(viewPath: "home.index");
+//        $listTop10BestSeller // Lấy top 10  sản phẩm có best_sell = 1
+//        $listFAQs // Lấy tất cả trong FAQs
+        return $this->view(viewPath: "home.index", params: [
+            "listTop10BestSeller" => $listTop10BestSeller ?? null,
+            "listFAQs" => $listFAQs ?? null,
+        ]);
     }
 }
