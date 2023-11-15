adad28775c0651b327c42fcfa173e4c4e399f033
ngocps35536 <doanvanngoc061220@gmail.com>
2023-11-15 14:43:29 +0700

    add comment ProductController

--- app/models/ProductModel.php
+++ app/models/ProductModel.php
@@ -5,6 +5,8 @@ class ProductModel extends BaseModel
     const PROD_CAT = "categories";
     const PROD = "products";
     const PROD_STOCK = "product_stocks";
+    const PROD_CMT = "product_comments";
+    const USER = "users";
 
     public function getProductDetails($id) {
         $productData = $this->getOne(table: self::PROD, conditions: [
@@ -32,4 +34,15 @@ class ProductModel extends BaseModel
         return $catName;
     }
 
+    public function getComments($id) {
+        $productComment = $this->getOne(table: self::PROD_CMT, conditions: [
+            "prod_id" => $id,
+        ]);
+
+        $productComment2 = $this->getTwoTable(table1: self::PROD_CMT, table2: self::USER,
+                                            joinColumn: "user_id", table1Select: ["comment_text", "comment_time"], table2Select: ["username"], conditions: [
+                                                "prod_id" => $id,
+            ]);
+        return $productComment2;
+    }
 }
