47947271f5f044e032fa70ae7c7513a81a0d6158
ngocps35536 <doanvanngoc061220@gmail.com>
2023-11-22 16:35:55 +0700

    Update viewDefault.php

--- app/views/product/viewDefault.php
+++ app/views/product/viewDefault.php
@@ -3,4 +3,12 @@ print_r($productData);
 echo "<br>";
 print_r($listRateData); 
 echo "<br>";
-print_r($listComment);
+print_r($listComment); 
+
+print_r($rateScore);
+echo "<br>";
+
+foreach($rateScore as $sp){
+    $tb=array_sum($sp)/count($sp);
+}
+echo "Đánh giá tb: ".$tb;
