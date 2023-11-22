25fe23b3111ba6cb936824d9b69b70fdd229b86f
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2023-11-22 17:51:15 +0700

    index  page

--- app/views/product/viewDefault.php
+++ app/views/product/viewDefault.php
@@ -1,1 +1,22 @@
-<?php
+
+
+<main class="container-fluid d-flex flex-column justify-content-center align-items-center p-5">
+    <div class="container row row-cols-md-2 row-cols-sm-1 rounded-3 overflow-hidden">
+        <div class="col bg-white p-5">
+            <img src="<?php echo BASEPATH . "/public/img/test-img.png" ?>" alt="">
+        </div>
+        <div class="col" style="background-color: var(--purple-100)">
+            <div class="product-header">
+                <h2 class="heading-2">Lorem Ipsum is simply dummy text
+                    of the printing and typesetting
+                    industry.</h2>
+                <div>
+                    <a href="#" class="text-decoration-none badge rounded-pill p-2 bg-secondary"><p class="color-white m-0 text-small">Tên loại bánh</p></a>
+                    <a href="#" class="text-decoration-none badge rounded-pill p-2 bg-secondary"><p class="color-white m-0 text-small">4/5 Điểm đánh giá (9999 Lượt đánh giá)</p></a>
+                    <a href="#" class="text-decoration-none badge p-2 bg-secondary"><i class="color-white fa-regular fa-bookmark"></i></a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</main>
