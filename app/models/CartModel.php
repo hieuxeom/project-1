898c85c326914808c435057261f90ad45a925291
ngocps35536 <doanvanngoc061220@gmail.com>
2023-11-26 18:45:14 +0700

    UPDATE

--- app/models/CartModel.php
+++ app/models/CartModel.php
@@ -2,15 +2,15 @@
 
 class CartModel extends BaseModel
 {
-    const CART="carts";
-    const CART_IT="cart_items";
-    const USER="users";
-    const VOU="vouchers";
-    const PRO="products";
+    const CART_TABLE="carts";
+    const CART_ITEMS_TABLE="cart_items";
+    const USER_TABLE="users";
+    const VOUCHES_TABLE="vouchers";
+    const PROD_TABLE="products";
 
     // User với Cart
     public function getCart($userId){
-        $CartData=$this->getTwoTable(table1: self::USER, table2: self::CART,
+        $CartData=$this->getTwoTable(table1: self::USER_TABLE, table2: self::CART_TABLE,
                             joinColumn:"user_id", table1Select:["username",],
                              table2Select:["cart_id", "status", "paid_date","cart_total","address"],
                             conditions:["user_id"=>$userId]);
@@ -18,15 +18,15 @@ class CartModel extends BaseModel
     }
     // Showw cart
     public function getShowCart($cartId, $prodId){
-        $cart=$this->getOne(table: self::CART, conditions:["cart_id"=>$cartId]);
-        $showCart=$this->getTwoTable(table1: self::PRO, table2: self::CART_IT,
+        $cart=$this->getOne(table: self::CART_TABLE, conditions:["cart_id"=>$cartId]);
+        $showCart=$this->getTwoTable(table1: self::PROD_TABLE, table2: self::CART_ITEMS_TABLE,
         joinColumn:"prod_id", table1Select:["prod_name","img_path","prod_price"],
         table2Select:["quantity"], conditions:["prod_id"=>$prodId]);
         return $showCart;
     }
 // add khi chưa có sp
     public function addProduct($cartId,$productId,$quantity,$pad){
-        $addPro=$this->insert(table: self::CART_IT,
+        $addPro=$this->insert(table: self::CART_ITEMS_TABLE,
          data:["cart_id"=>$cartId,
          "prod_id"=>$productId,
          "quantity"=>$quantity,
@@ -35,21 +35,21 @@ class CartModel extends BaseModel
     }
 //add khi có sp
     public function addQuantityProduct($productId,$quantity){
-        $addQProd=$this->update(table: self::CART_IT, data:["quantity"=>$quantity],
+        $addQProd=$this->update(table: self::CART_ITEMS_TABLE, data:["quantity"=>$quantity],
         conditions:["prod_id"=>$productId]);
         return $addQProd;
     }
 
     // Xóa giỏ hàng
     public function delCart($cartId){
-        $delCart=$this->delete(table: self:: CART, conditions:["cart_id"=>$cartId]);
+        $delCart=$this->delete(table: self:: CART_TABLE, conditions:["cart_id"=>$cartId]);
         return $delCart;
 
     }
 
     // Xóa sp trong giỏ hàng
     public function delProductCart($productId){
-        $delProdCart=$this->delete(table: self::CART_IT,
+        $delProdCart=$this->delete(table: self::CART_ITEMS_TABLE,
         conditions:["prod_id"=>$productId]);
         return $delProdCart;
     }
