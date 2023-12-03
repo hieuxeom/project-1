3f71545fbbde4875dd5c25ad175ace17041abf47
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2023-12-03 22:08:16 +0700

    test inorge

--- config/config.php
+++ config/config.php
@@ -1,11 +1,11 @@
 <?php
-define('DB_HOST', "");
-define('DB_NAME', "");
-define('DB_USER', "");
-define('DB_PASS', "");
-define('MAIL_ID', "");
-define('MAIL_PASSWORD', "");
-define('STORE_NAME', "");
+define('DB_HOST', "103.221.221.104");
+define('DB_NAME', "kdjxkbin_pro1014");
+define('DB_USER', "kdjxkbin_admin");
+define('DB_PASS', "Hieuxeom@123");
+define('MAIL_ID', "hieutn.bedev@gmail.com");
+define('MAIL_PASSWORD', "mgkb ftcy hdrs bidi");
+define('STORE_NAME', "ShopBake");
 
 $server = 1;
 
